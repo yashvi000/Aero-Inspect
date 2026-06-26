3fedd018251ed917791b89f8afea52e0a5d43953
Shankhdhardaksh <46623152+Shankhdhardaksh@users.noreply.github.com>
2026-06-26 20:58:16 +0530

    feat: add zone seed script and update session to use env vars

--- src/backend/db/session.py
+++ src/backend/db/session.py
@@ -5,7 +5,13 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
 
